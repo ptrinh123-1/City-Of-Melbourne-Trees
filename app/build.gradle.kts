1938c34698ebb6dac380d10fa4a6b93948ad825a
ptrinh123-1 <101618268@student.swin.edu.au>
2025-09-26 15:43:33 +1000

    remove room dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.ksp)
 }
 
 android {
@@ -65,10 +64,6 @@ dependencies {
     implementation(libs.coil.compose)
 
     implementation(libs.androidx.lifecycle.viewmodel.compose)
-
-    implementation(libs.room.runtime)
-    implementation(libs.room.ktx)
-    ksp(libs.room.compiler)
 
     implementation(libs.retrofit)
     implementation(libs.retrofit.converter.gson)
